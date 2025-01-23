3f43c674464b3fff8f46802000548ae19b265b51
Odinsson25 <64733407+Odinsson25@users.noreply.github.com>
2025-01-23 14:43:10 +0100

    added support server for developer commands

--- src/index.ts
+++ src/index.ts
@@ -11,34 +11,34 @@ import { fileURLToPath } from "node:url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const client = new Client({
-  intents: [
-    IntentsBitField.Flags.Guilds,
-    IntentsBitField.Flags.GuildMembers,
-    IntentsBitField.Flags.GuildMessages,
-    IntentsBitField.Flags.MessageContent,
-  ],
+	intents: [
+		IntentsBitField.Flags.Guilds,
+		IntentsBitField.Flags.GuildMembers,
+		IntentsBitField.Flags.GuildMessages,
+		IntentsBitField.Flags.MessageContent,
+	],
 });
 
 new CommandKit({
-  client,
-  eventsPath: join(__dirname, "events"),
-  commandsPath: join(__dirname, "commands"),
-  devGuildIds: [jsonConfig.testServer],
-  devUserIds: jsonConfig.devs as string[],
-  devRoleIds: [jsonConfig.roles.dev],
+	client,
+	eventsPath: join(__dirname, "events"),
+	commandsPath: join(__dirname, "commands"),
+	devGuildIds: [jsonConfig.testServer, "952501691532587068"], // + support server
+	devUserIds: jsonConfig.devs as string[],
+	devRoleIds: [jsonConfig.roles.dev],
 });
 
 (async () => {
-  try {
-    console.log("🔁 Connecting to database...");
-    mongoose.set("strictQuery", false);
-    await mongoose
-      .connect(process.env.MONGO_URI as string)
-      .then(() => console.log("✅ Connected to database."));
-  } catch (error) {
-    console.log("❌ Connection to database failed!");
-    console.log(error);
-  }
+	try {
+		console.log("🔁 Connecting to database...");
+		mongoose.set("strictQuery", false);
+		await mongoose
+			.connect(process.env.MONGO_URI as string)
+			.then(() => console.log("✅ Connected to database."));
+	} catch (error) {
+		console.log("❌ Connection to database failed!");
+		console.log(error);
+	}
 })();
 
 client.login(process.env.TOKEN);
