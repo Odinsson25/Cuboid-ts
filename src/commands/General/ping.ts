aee6c0a78a14b757812325b4337d60308333361d
Odinsson25 <64733407+Odinsson25@users.noreply.github.com>
2025-01-25 00:42:35 +0100

    made async for copying this file

--- src/commands/General/ping.ts
+++ src/commands/General/ping.ts
@@ -1,18 +1,18 @@
 import type {
-  CommandData,
-  SlashCommandProps,
-  CommandOptions,
+	CommandData,
+	SlashCommandProps,
+	CommandOptions,
 } from "commandkit";
 
 export const data: CommandData = {
-  name: "ping",
-  description: "Replies with Pong",
+	name: "ping",
+	description: "Replies with Pong",
 };
 
-export const run = ({ interaction, handler }: SlashCommandProps) => {
-  interaction.reply("Pong!");
+export const run = async ({ interaction, handler }: SlashCommandProps) => {
+	interaction.reply("Pong!");
 };
 
 export const options: CommandOptions = {
-  // https://commandkit.dev/docs/types/CommandOptions
+	// https://commandkit.dev/docs/types/CommandOptions
 };
