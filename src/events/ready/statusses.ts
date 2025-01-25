a8de439c2eef96e96b999c1c9cb5961966d775bd
Odinsson25 <64733407+Odinsson25@users.noreply.github.com>
2025-01-26 00:17:44 +0100

    removed Breda statusses, added temp statusses for cuboid

--- src/events/ready/statusses.ts
+++ src/events/ready/statusses.ts
@@ -3,42 +3,38 @@ import * as jsonConfig from "../../../config.json";
 
 import type { Client } from "discord.js";
 export default (client: Client<true>) => {
-  let statusses = [
-    {
-      name: "Welkom in Breda!",
-      type: ActivityType.Custom,
-    },
-    {
-      name: "BredaV2!",
-      type: ActivityType.Custom,
-    },
-    {
-      name: "met de spelers",
-      type: ActivityType.Playing,
-    },
-    {
-      name: `Join de game! Code: `,
-      type: ActivityType.Custom,
-    },
-    {
-      name: "Invite je vrienden om mee te spelen!",
-      type: ActivityType.Custom,
-    },
-    {
-      name: "Hulp nodig? Open een ticket!",
-      type: ActivityType.Custom,
-    },
-    {
-      name: "Server link in beschrijving!",
-      type: ActivityType.Custom,
-    },
-  ];
+	let statusses = [
+		{
+			name: "Cuboid Development",
+			type: ActivityType.Custom,
+		},
+		{
+			name: "© Cuboid",
+			type: ActivityType.Custom,
+		},
+		{
+			name: `Join de support Server!`,
+			type: ActivityType.Custom,
+		},
+		{
+			name: "Invite mij in jouw server!",
+			type: ActivityType.Custom,
+		},
+		{
+			name: "Hulp nodig? Open een ticket!",
+			type: ActivityType.Custom,
+		},
+		{
+			name: "Server link in beschrijving!",
+			type: ActivityType.Custom,
+		},
+	];
 
-  let statusCount = -1;
-  setInterval(() => {
-    statusCount++;
-    if (statusCount === statusses.length) statusCount = 0;
+	let statusCount = -1;
+	setInterval(() => {
+		statusCount++;
+		if (statusCount === statusses.length) statusCount = 0;
 
-    client.user?.setActivity(statusses[statusCount]);
-  }, 5 * 1000);
+		client.user?.setActivity(statusses[statusCount]);
+	}, 5 * 1000);
 };
