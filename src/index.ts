33a99b45c7bf1882a7ea6677eb1aed8834c79109
Odinsson25 <64733407+Odinsson25@users.noreply.github.com>
2025-04-03 14:06:25 +0200

    Added anti-crash

--- src/index.ts
+++ src/index.ts
@@ -29,6 +29,16 @@ new CommandKit({
 	devRoleIds: [jsonConfig.roles.dev],
 });
 
+process.on("unhandledRejection", (reason, promise) => {
+	console.log("Unhandled Rejection Error");
+	console.log(reason, promise);
+}); // Handles Unhandled Rejection Errors
+
+process.on("uncaughtException", (err, origin) => {
+	console.log("Uncaught Exeception Error");
+	console.log(err, origin);
+}); // Handles Uncaught Excpetion Errors
+
 (async () => {
 	try {
 		console.log("🔁 Connecting to database...");
