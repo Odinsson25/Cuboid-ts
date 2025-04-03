cf0f79dd772b1c299322481bec7b213ee246bebb
Odinsson25 <64733407+Odinsson25@users.noreply.github.com>
2025-04-03 19:17:06 +0200

    started on warn function

---
+++ src/functions/warn.ts
@@ -0,0 +1,7 @@
+import { GuildMember } from "discord.js";
+import type { Guild } from "discord.js";
+
+export default async (member: GuildMember, guild: Guild | string) => {
+	// if(typeof guild === Guild) {g}
+	return;
+};
